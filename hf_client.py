3e52e7ef2337e1c119df845cd42f62de9755a8e7
ArvindNandigam <nandigamarvind@gmail.com>
2026-03-01 21:45:18 +0530

    using Qwen/Qwen-Image

--- hf_client.py
+++ hf_client.py
@@ -50,7 +50,7 @@ client = InferenceClient(
 def generate_image(prompt: str):
     image = client.text_to_image(
         prompt,
-        model="stabilityai/stable-diffusion-2-1",
+        model="Qwen/Qwen-Image",
         width=512,
         height=512,
         num_inference_steps=30,
